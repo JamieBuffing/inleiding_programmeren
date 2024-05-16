70bed59347314fc2805e30a92b96e4104e754523
Jamie Buffing <jamiebuffing@gmail.com>
2024-05-16 12:31:58 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,98 @@
-document.querySelector("#info").textContent = "test"
+const l1_0 = document.querySelector("#L1_0");
+const l1_1 = document.querySelector("#L1_1");
+const l1_2 = document.querySelector("#L1_2");
+const l1_3 = document.querySelector("#L1_3");
+const l1_4 = document.querySelector("#L1_4");
+const l1_5 = document.querySelector("#L1_5");
+
+const l2_0 = document.querySelector("#L2_0");
+const l2_1 = document.querySelector("#L2_1");
+const l2_2 = document.querySelector("#L2_2");
+const l2_3 = document.querySelector("#L2_3");
+const l2_4 = document.querySelector("#L2_4");
+const l2_5 = document.querySelector("#L2_5");
+
+const l3_0 = document.querySelector("#L3_0");
+const l3_1 = document.querySelector("#L3_1");
+const l3_2 = document.querySelector("#L3_2");
+const l3_3 = document.querySelector("#L3_3");
+const l3_4 = document.querySelector("#L3_4");
+const l3_5 = document.querySelector("#L3_5");
+
+function f1_0 () {
+    console.log ("1_geen optie")
+}
+function f1_1 () {
+    console.log ("1_optie 1")
+}
+function f1_2 () {
+    console.log ("1_optie 2")
+}
+function f1_3 () {
+    console.log ("1_optie 3")
+}
+function f1_4 () {
+    console.log ("1_optie 4")
+}
+function f1_5 () {
+    console.log ("1_optie 5")
+}
+
+function f2_0 () {
+    console.log ("2_geen optie")
+}
+function f2_1 () {
+    console.log ("2_optie 1")
+}
+function f2_2 () {
+    console.log ("2_optie 2")
+}
+function f2_3 () {
+    console.log ("2_optie 3")
+}
+function f2_4 () {
+    console.log ("2_optie 4")
+}
+function f2_5 () {
+    console.log ("2_optie 5")
+}
+
+function f3_0 () {
+    console.log ("3_geen optie")
+}
+function f3_1 () {
+    console.log ("3_optie 1")
+}
+function f3_2 () {
+    console.log ("3_optie 2")
+}
+function f3_3 () {
+    console.log ("3_optie 3")
+}
+function f3_4 () {
+    console.log ("3_optie 4")
+}
+function f3_5 () {
+    console.log ("3_optie 5")
+}
+
+l1_0.addEventListener('click', f1_0)
+l1_1.addEventListener('click', f1_1)
+l1_2.addEventListener('click', f1_2)
+l1_3.addEventListener('click', f1_3)
+l1_4.addEventListener('click', f1_4)
+l1_5.addEventListener('click', f1_5)
+
+l2_0.addEventListener('click', f2_0)
+l2_1.addEventListener('click', f2_1)
+l2_2.addEventListener('click', f2_2)
+l2_3.addEventListener('click', f2_3)
+l2_4.addEventListener('click', f2_4)
+l2_5.addEventListener('click', f2_5)
+
+l3_0.addEventListener('click', f3_0)
+l3_1.addEventListener('click', f3_1)
+l3_2.addEventListener('click', f3_2)
+l3_3.addEventListener('click', f3_3)
+l3_4.addEventListener('click', f3_4)
+l3_5.addEventListener('click', f3_5)
